4576457ab573d5788bc14e9643ce6562965e05d2
shubhamp489 <shubhamp489@gmail.com>
2024-03-04 22:22:02 +0530

    Isomorphic string

--- LeetCodeDataStructure/src/dynamicProgrammingPattern/Practice.java
+++ LeetCodeDataStructure/src/dynamicProgrammingPattern/Practice.java
@@ -1,11 +1,44 @@
 package dynamicProgrammingPattern;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Practice {
-	public static void main(String args[])
-	{
-	int i=20+ +9- -12+ +4- -13+ +19;
+	
 
-	System.out.println(i);
+	public static void main(String args[]) {
+		String s = "egg";
+		String t = "add";
+		int temp=0;
+		HashMap<Character, Integer> map1 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
 
-	}
+		
+		
+		for (char data1 : s.toCharArray()) {
+			if (map1.containsKey(data1)) {
+				map1.put(data1, map1.get(data1) + 1);
+			}
+
+			else {
+				map1.put(data1, 1);
+			}
+		}
+			
+			for (char data2 : t.toCharArray()) {
+				if (map1.containsKey(data2)) {
+					map1.put(data2, map1.get(data2) + 1);
+				}
+
+				else {
+					map1.put(data2, 1);
+				}
+			}
+			
+			
+			}
 }
